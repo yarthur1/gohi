2aa95f52c347a53ddb4151a1816b28fe7bf04fe4
yexijun <yexijun@joyme.sg>
2021-02-01 17:41:27 +0800

    科学计数 parsefloat

--- main/scientificcount.go
+++ main/scientificcount.go
@@ -2,17 +2,25 @@ package main
 import (
     "fmt"
     "github.com/shopspring/decimal"
+    "strconv"
 )
 // https://blog.csdn.net/LinHenk/article/details/88821297
 
 func main(){
-    fmt.Println(int64(8.04758523075822e+17))  //科学计数法可以直接转化为int64
+    fmt.Println(int64(8.04758523075822e+17))  //科学计数可以直接转化为int64  科学计数法字符串不能直接转int64
+    numStr := "8.04758523075822e+17"          //科学计数法字符串
+
+    newNum1, err := strconv.ParseInt(numStr, 10, 64)   //科学计数法字符串不能直接转int64
+    if err != nil{
+        fmt.Println(err)
+    }
+    fmt.Println(newNum1)
 
     var newNum float64
-    numStr := "8.04758523075822e+17"          //科学计数法字符串
-    _, err := fmt.Sscanf(numStr, "%e", &newNum)
+
+    _, err = fmt.Sscanf(numStr, "%e", &newNum)
     if err != nil {
-        return
+        fmt.Println(err)
     }
     fmt.Println(newNum)
 
@@ -22,8 +30,15 @@ func main(){
 
     decimalNum, err := decimal.NewFromString(numStr)
     if err != nil {
-        return
+        fmt.Println(err)
     }
     fmt.Println(decimalNum.String())   //精度不变
+
+    newNum2, err := strconv.ParseFloat(numStr, 64)   //科学计数法字符串可以直接转float64,精度会变小
+    if err != nil{
+        fmt.Println(err)
+    }
+    fmt.Println(newNum2)
+    fmt.Println(int64(newNum2))
 
 }
